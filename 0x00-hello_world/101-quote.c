38a290d40705a76134ad735789a28ae901830d3a
Rayanmo2121 <me@example.com>
2023-07-18 17:47:45 -0700

    k

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,17 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ *      write(int fd,const void *buf,size_t count)
+ *
+ * Return: 1 (error)
+*/
+
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59);
-    return 1;
+char que[] -"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59);
+return (1);
 }
